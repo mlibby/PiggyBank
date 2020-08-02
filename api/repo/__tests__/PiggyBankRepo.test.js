b0b8b7e48b659963fca22543491006ad223d5e91
Michael C. Libby <m@mlibby.com>
2020-08-02 17:00:12 -0500

    some improvements in PiggyBankRepo.test.js

--- api/repo/__tests__/PiggyBankRepo.test.js
+++ api/repo/__tests__/PiggyBankRepo.test.js
@@ -1,55 +1,64 @@
-const migrationDir = "migrations"
-const migration00001 = "00001_migration.sql"
-const migration00002 = "00002_account.sql"
-const migration00003 = "00003_tx.sql"
-const migration00004 = "00004_budget.sql"
-const path00001 = "migrations/00001_migration.sql"
-const path00002 = "migrations/00002_account.sql"
-const path00003 = "migrations/00003_tx.sql"
-const path00004 = "migrations/00004_budget.sql"
-const sql00001 = "select * from migrations"
-const sql00002 = "select * from accounts"
-const sql00003 = "select * from tx"
-const sql00004 = "select * from budget"
+const mockMigrationDir = "migrations"
+const mockMigration00001 = "00001_migration.sql"
+const mockMigration00002 = "00002_account.sql"
+const mockMigration00003 = "00003_tx.sql"
+const mockMigration00004 = "00004_budget.sql"
+const mockPath00001 = "migrations/00001_migration.sql"
+const mockPath00002 = "migrations/00002_account.sql"
+const mockPath00003 = "migrations/00003_tx.sql"
+const mockPath00004 = "migrations/00004_budget.sql"
+const mockSql00001 = "select * from migrations"
+const mockSql00002 = "select * from accounts"
+const mockSql00003 = "select * from tx"
+const mockSql00004 = "select * from budget"
 
-jest.mock("better-sqlite3")
-const SQLite3 = require("better-sqlite3")
-const db = new SQLite3(":memory:")
-
-db.prepare = jest.fn()
-db.get = jest.fn()
-db.pluck = jest.fn().mockReturnValue(this)
-
-const fs = jest.mock("fs", () => {
+jest.mock("fs", () => {
   return {
-    readdir: jest.fn()
-      .mockReturnValueOnce([
-        migration00001,
-        migration00002,
-        migration00003,
-        migration00004
+    readdirSync: jest.fn()
+      .mockReturnValue([
+        mockMigration00001,
+        mockMigration00002,
+        mockMigration00003,
+        mockMigration00004
       ]),
-    readfile: jest.fn()
-      .mockReturnValueOnce(sql00001)
-      .mockReturnValueOnce(sql00002)
-      .mockReturnValueOnce(sql00003)
-      .mockReturnValueOnce(sql00004)
+    readfileSync: jest.fn()
+      .mockReturnValueOnce(mockSql00001)
+      .mockReturnValueOnce(mockSql00002)
+      .mockReturnValueOnce(mockSql00003)
+      .mockReturnValueOnce(mockSql00004)
   }
 })
 
-const path = jest.mock("path", () => {
+jest.mock("path", () => {
   return {
-    join: jest.fn().mockReturnValueOnce(migrationDir)
-      .mockReturnValueOnce(path00001)
-      .mockReturnValueOnce(path00002)
-      .mockReturnValueOnce(path00003)
-      .mockReturnValueOnce(path00004)
+    join: jest.fn().mockReturnValueOnce(mockMigrationDir)
+      .mockReturnValueOnce(mockPath00001)
+      .mockReturnValueOnce(mockPath00002)
+      .mockReturnValueOnce(mockPath00003)
+      .mockReturnValueOnce(mockPath00004)
   }
 })
+
+class mockSQLite3 {
+  constructor() { }
+  pragma() { return this }
+  prepare() { return this }
+  get() { return this }
+  pluck() { return this }
+}
+
+jest.mock("better-sqlite3", () => mockSQLite3)
+const SQLite3 = require("better-sqlite3")
+const db = new SQLite3()
+
+const path = require("path")
+const fs = require("fs")
 
 const { PiggyBankRepo } = require("../PiggyBankRepo")
 
-test("getCurrentLevel() returns 0 when no migrations have run yet", async () => {
+test("getCurrentLevel() returns 0 when no migrations have run yet", () => {
+  db.prepare = jest.fn().mockReturnValue(db)
+  
   db.get = jest.fn().mockImplementation(() => {
     throw new Error('Error: no such table: migrations')
   })
@@ -60,21 +69,20 @@ test("getCurrentLevel() returns 0 when no migrations have run yet", async () => 
   expect(level).toBe(0)
 })
 
-test("getCurrentLevel() > 0 once migrations have been run", async () => {
-  client.query = jest.fn().mockResolvedValue({ rows: [{ max: "1" }] })
+test("getCurrentLevel() > 0 once migrations have been run", () => {
   const repo = new PiggyBankRepo()
-  const level = await repo.getMigrationLevel()
-  expect(client.query).toHaveBeenCalledWith('select max(level) from migration', undefined)
+  const level = repo.getMigrationLevel()
+  //expect(client.query).toHaveBeenCalledWith('select max(level) from migration', undefined)
   expect(level).toBe(1)
 })
 
-test("getCurrentLevel() only swallows 'migrations' missing error", async () => {
-  client.query = jest.fn().mockImplementation(() => {
+test("getCurrentLevel() only swallows 'migrations' missing error", () => {
+  db.get = jest.fn().mockImplementation(() => {
     throw new Error("this is not a migrations error")
   })
   const repo = new PiggyBankRepo()
-  expect(async () => {
-    const level = await repo.getMigrationLevel()
+  expect(() => {
+    const level = repo.getMigrationLevel()
   }).rejects.toThrow()
 })
 
@@ -93,70 +101,67 @@ test("updateDb() gets the current migration level", () => {
 })
 
 test("updateDb() gets the file list from migrations dir", () => {
-  path.join.mockReturnValue(migrationDir)
-  client.query = jest.fn().mockResolvedValue({ rows: [{ max: "1" }] })
-
+  path.join.mockReturnValue(mockMigrationDir)
+  
   const repo = new PiggyBankRepo()
   repo.updateDb()
 
-  expect(readdir).toHaveBeenCalledTimes(1)
-  expect(readdir).toHaveBeenCalledWith(migrationDir)
+  expect(fs.readdirSync).toHaveBeenCalledTimes(1)
+  expect(fs.readdirSync).toHaveBeenCalledWith(mockMigrationDir)
 })
 
 test("updateDb() runs all migrations at migration level 0", () => {
-
   const repo = new PiggyBankRepo()
   repo.query = jest.fn()
   repo.getMigrationLevel = jest.fn().mockResolvedValueOnce(0)
   repo.updateDb()
 
   expect(path.join).toHaveBeenCalledTimes(5)
-  expect(path.join.mock.calls[1]).toEqual([migrationDir, migration00001])
-  expect(path.join.mock.calls[2]).toEqual([migrationDir, migration00002])
-  expect(path.join.mock.calls[3]).toEqual([migrationDir, migration00003])
-  expect(path.join.mock.calls[4]).toEqual([migrationDir, migration00004])
+  expect(path.join.mock.calls[1]).toEqual([mockMigrationDir, mockMigration00001])
+  expect(path.join.mock.calls[2]).toEqual([mockMigrationDir, mockMigration00002])
+  expect(path.join.mock.calls[3]).toEqual([mockMigrationDir, mockMigration00003])
+  expect(path.join.mock.calls[4]).toEqual([mockMigrationDir, mockMigration00004])
 
   expect(readfile).toHaveBeenCalledTimes(4)
-  expect(readfile.mock.calls[0]).toEqual([path00001])
-  expect(readfile.mock.calls[1]).toEqual([path00002])
-  expect(readfile.mock.calls[2]).toEqual([path00003])
-  expect(readfile.mock.calls[3]).toEqual([path00004])
+  expect(readfile.mock.calls[0]).toEqual([mockPath00001])
+  expect(readfile.mock.calls[1]).toEqual([mockPath00002])
+  expect(readfile.mock.calls[2]).toEqual([mockPath00003])
+  expect(readfile.mock.calls[3]).toEqual([mockPath00004])
 
   // would expect 5 queries, but we're mocking getMigrationLevel()
   expect(repo.query).toHaveBeenCalledTimes(4)
-  expect(repo.query.mock.calls[0]).toEqual([sql00001])
-  expect(repo.query.mock.calls[1]).toEqual([sql00002])
-  expect(repo.query.mock.calls[2]).toEqual([sql00003])
-  expect(repo.query.mock.calls[3]).toEqual([sql00004])
+  expect(repo.query.mock.calls[0]).toEqual([mockSql00001])
+  expect(repo.query.mock.calls[1]).toEqual([mockSql00002])
+  expect(repo.query.mock.calls[2]).toEqual([mockSql00003])
+  expect(repo.query.mock.calls[3]).toEqual([mockSql00004])
 })
 
-test("updateDb() runs only pending migrations at level 2", async () => {
+test("updateDb() runs only pending migrations at level 2", () => {
   path.join = jest.fn()
-    .mockReturnValueOnce(migrationDir)
-    .mockReturnValueOnce(path00003)
-    .mockReturnValueOnce(path00004)
+    .mockReturnValueOnce(mockMigrationDir)
+    .mockReturnValueOnce(mockPath00003)
+    .mockReturnValueOnce(mockPath00004)
 
-  readfile = jest.fn()
-    .mockReturnValueOnce(sql00003)
-    .mockReturnValueOnce(sql00004)
+  fs.readfileSync = jest.fn()
+    .mockReturnValueOnce(mockSql00003)
+    .mockReturnValueOnce(mockSql00004)
 
   const repo = new PiggyBankRepo()
-  repo.query = jest.fn()
   repo.getMigrationLevel = jest.fn().mockResolvedValueOnce(2)
-  await repo.updateDb()
+  repo.updateDb()
 
   expect(path.join).toHaveBeenCalledTimes(3)
 
-  expect(path.join.mock.calls[1]).toEqual([migrationDir, migration00003])
-  expect(path.join.mock.calls[2]).toEqual([migrationDir, migration00004])
+  expect(path.join.mock.calls[1]).toEqual([mockMigrationDir, mockMigration00003])
+  expect(path.join.mock.calls[2]).toEqual([mockMigrationDir, mockMigration00004])
 
-  expect(readfile).toHaveBeenCalledTimes(2)
-  expect(readfile.mock.calls[0]).toEqual([path00003])
-  expect(readfile.mock.calls[1]).toEqual([path00004])
+  expect(fs.readfileSync).toHaveBeenCalledTimes(2)
+  expect(fs.readfileSync.mock.calls[0]).toEqual([mockPath00003])
+  expect(fs.readfileSync.mock.calls[1]).toEqual([mockPath00004])
 
   expect(repo.query).toHaveBeenCalledTimes(2)
-  expect(repo.query.mock.calls[0]).toEqual([sql00003])
-  expect(repo.query.mock.calls[1]).toEqual([sql00004])
+  expect(repo.query.mock.calls[0]).toEqual([mockSql00003])
+  expect(repo.query.mock.calls[1]).toEqual([mockSql00004])
 })
 
 test("repo.account is an AccountRepo", () => {
