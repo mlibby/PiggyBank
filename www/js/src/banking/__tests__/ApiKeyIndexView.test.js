f068c3433986bb104eef4a317f57dfca4570c6fc
Michael C. Libby <m@mlibby.com>
2020-12-30 13:00:50 -0600

    ✅ finish test for ApiKeyIndexView

--- www/js/src/banking/__tests__/ApiKeyIndexView.test.js
+++ www/js/src/banking/__tests__/ApiKeyIndexView.test.js
@@ -7,6 +7,15 @@ beforeEach(() => {
 })
 
 test("ApiKeyIndexView.fetchSuccess() builds table of models from collection", () => {
+  const mockAppend = jest.fn()
+  view.$el.find = jest.fn(() => {
+    return {
+      append: mockAppend
+    }
+  })
+
   view.fetchSuccess(mockApiKeys)
-  expect(false).toBe(true)
+
+  expect(view.$el.find).toHaveBeenCalledWith("table")
+  expect(mockAppend).toHaveBeenCalled()
 })
