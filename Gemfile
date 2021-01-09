bb0dd7f4cfa9cb2e00576e8036e4bc2caaf7662f
Michael C. Libby <m@mlibby.com>
2021-01-08 22:31:36 -0600

    ➕ New gems depended on: ofx (forked), sequel, sqlite3

--- Gemfile
+++ Gemfile
@@ -4,4 +4,8 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
+
+gem "ofx", git: "https://github.com/mlibby/ofx.git"
+gem "sequel"
 gem "sinatra"
+gem "sqlite3"
--- server.rb
+++ server.rb
@@ -1,5 +1,14 @@
 require "sinatra"
+require "sequel"
+require "sqlite3"
+
+DB = Sequel.connect("sqlite://piggybank.db")
 
 get "/" do 
   "Oink! Oink!"
+end
+
+get "/account" do
+  accounts = DB[:account].all.to_s
+  accounts
 end
