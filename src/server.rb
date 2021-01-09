5c0b5737d03a20f2b09b7233fe6fcfb84ef97998
Michael C. Libby <m@mlibby.com>
2021-01-09 13:07:56 -0600

    migrate migrations

--- src/server.rb
+++ src/server.rb
@@ -3,6 +3,8 @@ require "sequel"
 require "sqlite3"
 
 DB = Sequel.connect("sqlite://piggybank.db")
+Sequel.extension :migration
+Sequel::Migrator.run(DB, "src/db/migrations")
 
 get "/" do
   "Oink! Oink!"
