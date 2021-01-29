aa16c73baf3577aac6aebd0ee4aec3f3d0f749be
Michael C. Libby <m@mlibby.com>
2021-01-29 15:08:09 -0600

    ✨ basic ApiKey model stuff

--- src/repo/models/api_key.rb
+++ src/repo/models/api_key.rb
@@ -1,22 +1,23 @@
 module PiggyBank
-
+  # primary_key :api_key_id
+  # String :description, text: true, null: false
+  # String :value, text: true, null: false
+  # String :version, text: true, null: false
   class ApiKey < Sequel::Model(:api_key)
     plugin :validation_helpers
 
-    # one_to_many :splits, class: PiggyBank::Split
+    def self.update_fields
+      return [:description, :value]
+    end
 
-    # def self.update_fields
-    #   return [:post_date, :number, :description]
-    # end
+    def before_create
+      self.version = PiggyBank::Repo.timestamp
+    end
 
-    # def before_create
-    #   self.version = PiggyBank::Repo.timestamp
-    # end
-
-    # def validate
-    #   super
-    #   validates_presence :post_date
-    #   validates_presence :description
-    # end
+    def validate
+      super
+      validates_presence :description
+      validates_presence :value
+    end
   end
 end
