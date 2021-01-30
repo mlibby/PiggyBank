d47c295d752d0f1be4ea220a4c393456777a02f8
Michael C. Libby <m@mlibby.com>
2021-01-30 13:18:58 -0600

    ✨ Split validation

--- spec/models/split_spec.rb
+++ spec/models/split_spec.rb
@@ -7,58 +7,38 @@ describe PiggyBank::Split do
     seed_db
   end
 
-  #context "new instance" do
-  #let(:instance) { PiggyBank::Split.new }
+  context "new split" do
+    let(:split) { PiggyBank::Split.new }
+    let(:assets) { PiggyBank::Account.find(name: "Assets") }
+    let(:tx) { PiggyBank::Tx.first }
 
-  # it "is invalid at first" do
-  #   expect(true).to be true
-  #   expect(instance.valid?).to be false
-  # end
+    it "is invalid at first" do
+      expect(split.valid?).to be false
+    end
 
-  # it "is valid after attributes set" do
-  #   instance.name = "Checking"
-  #   instance.type = PiggyBank::Split::TYPE[:asset]
-  #   instance.parent = PiggyBank::Split.find(name: "Assets")
-  #   instance.commodity = PiggyBank::Commodity.find(name: "USD")
-  #   instance.is_placeholder = false
-  #   expect(instance.valid?).to be true
-  # end
+    it "is valid after attributes set" do
+      split.tx_id = tx.tx_id
+      split.memo = "split memo"
+      split.account_id = assets.account_id
+      split.amount = 12.23
+      split.value = 12.23
+      expect(split.valid?).to be true
+    end
 
-  # it "has a version after save" do
-  #   instance.name = "Checking"
-  #   instance.parent_id = assets.split_id
-  #   instance.commodity_id = usd.commodity_id
-  #   instance.is_placeholder = false
-  #   instance.type = PiggyBank::Split::TYPE[:asset]
-  #   instance.save
+    it "has a version after save" do
+      split.tx_id = tx.tx_id
+      split.memo = "split memo"
+      split.account_id = assets.account_id
+      split.amount = 12.23
+      split.value = 12.23
+      split.save
 
-  #   expect(instance.version).to match(/\d{4}\-\d\d-\d\dT\d\d:\d\d:\d\d\+00:00/)
-  # end
-  # end
+      expect(split.version).to match(/\d{4}\-\d\d-\d\dT\d\d:\d\d:\d\d\+00:00/)
+    end
+  end
 
-  # context "existing instance" do
-  #   it "Split#parent" do
-  #     mortgage = PiggyBank::Split.find(name: "Mortgage")
-  #     expect(mortgage.parent.name).to eq "Liabilities"
-  #   end
-
-  #   it "Split#long_name" do
-  #     mortgage = PiggyBank::Split.find(name: "Mortgage")
-  #     expect(mortgage.long_name).to eq "Liabilities:Mortgage"
-  #   end
-
-  #   it "Split#split_opts has correct option selected" do
-  #     mortgage = PiggyBank::Split.find(name: "Mortgage")
-
-  #     split_opts = mortgage.split_opts
-  #     expect(split_opts.length).to eq 6
-
-  #     mortgage_opt = split_opts.find { |ao| ao[:value] == mortgage.split_id }
-  #     expect(mortgage_opt[:selected]).to eq true
-  #   end
-  #end
 end
 
-# TODO: split validation required
-# TODO: validate required attributes
-# TODO: version on save
+# ZZZ: split validation required
+# ZZZ: validate required attributes
+# ZZZ: version on save
--- src/repo/models/split.rb
+++ src/repo/models/split.rb
@@ -2,7 +2,7 @@ module PiggyBank
   #
   # primary_key :split_id
   # foreign_key :tx_id, :tx, null: false, key: [:tx_id]
-  # foreign_key :account_id, :account, null: false, key: [:account_id]  
+  # foreign_key :account_id, :account, null: false, key: [:account_id]
   # String :memo, text: true
   # BigDecimal :amount, null: false
   # BigDecimal :value, null: false
@@ -14,17 +14,20 @@ module PiggyBank
     many_to_one :tx, class: "PiggyBank::Tx"
     many_to_one :account, class: "PiggyBank::Account"
 
-    # def self.update_fields
-    #   return [:memo, :amount, :value]
-    # end
+    def self.update_fields
+      return [:tx_id, :account_id, :memo, :amount, :value]
+    end
 
     def before_create
       self.version = PiggyBank::Repo.timestamp
     end
 
-    # def validate
-    #   super
-    # end
+    def validate
+      validates_presence :tx_id
+      validates_presence :account_id
+      validates_presence :amount
+      validates_presence :value
+    end
   end
 end
 
