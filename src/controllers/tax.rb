f467fa3c70bb7b2e1ab69ff3ef5608f54f1ac3d9
Michael C. Libby <m@mlibby.com>
2021-01-30 15:16:49 -0600

    ✨ GET /tax/* pages added

--- src/controllers/tax.rb
+++ src/controllers/tax.rb
@@ -1,11 +1,31 @@
 module PiggyBank
   class App < Sinatra::Base
-    get "/tax" do
-      haml_layout :"tax/index"
+    get "/tax/data" do
+      haml_layout :"tax/data"
+    end
+
+    get "/tax/data/general" do
+      haml_layout :"tax/data/general"
+    end
+
+    get "/tax/data/income" do
+      haml_layout :"tax/data/income"
+    end
+
+    get "/tax/data/deduct" do
+      haml_layout :"tax/data/deduct"
+    end
+
+    get "/tax/data/tax" do
+      haml_layout :"tax/data/tax"
     end
 
     get "/tax/forms" do
       haml_layout :"tax/forms"
+    end
+
+    get "/tax/form/:unit/:form" do |unit, form|
+      haml_layout :"tax/form/#{unit}/#{form}"
     end
   end
 end
