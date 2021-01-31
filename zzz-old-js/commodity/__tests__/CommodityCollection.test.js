95dbb4cc2ed177fb59d039fef48ad7935f70ed45
Michael C. Libby <m@mlibby.com>
2021-01-31 10:06:04 -0600

    🔥 reorganize folder, delete straggler JS files

--- zzz-old-js/commodity/__tests__/CommodityCollection.test.js
+++
@@ -1,9 +0,0 @@
-import { CommodityCollection } from "../CommodityCollection"
-import { CommodityModel } from "../CommodityModel"
-
-test("CommodityCollection configuration", () => {
-  const collection = new CommodityCollection()
-  expect(collection.model).toBe(CommodityModel)
-  expect(collection.url).toBe("/api/commodity")
-  expect(collection.comparator).toBe("name")
-})
--- zzz-old-js/commodity/__tests__/CommodityModel.test.js
+++
@@ -1,6 +0,0 @@
-import { CommodityModel } from "../CommodityModel"
-
-test("CommodityModel  configuration", () => {
-  const model = new CommodityModel()
-  expect(model.urlRoot).toBe("/api/commodity")
-})
