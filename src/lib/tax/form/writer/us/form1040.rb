24916b3cda3575a5db8831c34b28a1fd49a54300
Michael C. Libby <m@mlibby.com>
2021-02-08 17:20:19 -0600

    ✨ dependents on 1040

--- src/lib/tax/form/writer/us/form1040.rb
+++ src/lib/tax/form/writer/us/form1040.rb
@@ -47,6 +47,39 @@ module PiggyBank
         }
       end
 
+      def dependent_fields
+        df = { text: {}, button: {} }
+        unless @general.dependents[0].nil?
+          df[:text]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow1[0].f1_16[0]"] = @general.dependents[0].name.upcase
+          df[:text]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow1[0].f1_17[0]"] = format_ssn(@general.dependents[0].ssn)
+          df[:text]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow1[0].f1_18[0]"] = @general.dependents[0].relation.upcase
+          df[:button]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow1[0].c1_13[0]"] = @general.dependents[0].child_credit
+          df[:button]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow1[0].c1_14[0]"] = @general.dependents[0].other_credit
+        end
+        unless @general.dependents[1].nil?
+          df[:text]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow2[0].f1_19[0]"] = @general.dependents[1].name.upcase
+          df[:text]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow2[0].f1_20[0]"] = format_ssn(@general.dependents[1].ssn)
+          df[:text]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow2[0].f1_21[0]"] = @general.dependents[1].relation.upcase
+          df[:button]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow2[0].c1_15[0]"] = @general.dependents[1].child_credit
+          df[:button]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow2[0].c1_16[0]"] = @general.dependents[1].other_credit
+        end
+        unless @general.dependents[2].nil?
+          df[:text]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow3[0].f1_22[0]"] = @general.dependents[2].name.upcase
+          df[:text]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow3[0].f1_23[0]"] = format_ssn(@general.dependents[2].ssn)
+          df[:text]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow3[0].f1_24[0]"] = @general.dependents[2].relation.upcase
+          df[:button]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow3[0].c1_17[0]"] = @general.dependents[2].child_credit
+          df[:button]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow3[0].c1_18[0]"] = @general.dependents[2].other_credit
+        end
+        unless @general.dependents[3].nil?
+          df[:text]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow4[0].f1_25[0]"] = @general.dependents[3].name.upcase
+          df[:text]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow4[0].f1_26[0]"] = format_ssn(@general.dependents[3].ssn)
+          df[:text]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow4[0].f1_27[0]"] = @general.dependents[3].relation.upcase
+          df[:button]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow4[0].c1_19[0]"] = @general.dependents[3].child_credit
+          df[:button]["topmostSubform[0].Page1[0].Table_Dependents[0].BodyRow4[0].c1_20[0]"] = @general.dependents[3].other_credit
+        end
+        df
+      end
+
       def button_fields
         {
           "topmostSubform[0].Page1[0].FilingStatus[0].c1_01[0]" => @general.filing_status == "single",
@@ -60,18 +93,18 @@ module PiggyBank
           "topmostSubform[0].Page1[0].c1_04[1]" => !@general.virtual,
           # "topmostSubform[0].Page1[0].c1_05[0]" => @general.dependent,
           # "topmostSubform[0].Page1[0].c1_06[0]" => @general.spouse_dependent,
-          # "topmostSubform[0].Page1[0].c1_06[0]" => @general.spouse_itemizes,
-          "topmostSubform[0].Page1[0].c1_07[0]" => @general.birthday < "1956-01-02",
-          "topmostSubform[0].Page1[0].c1_08[0]" => @general.blind,
-          "topmostSubform[0].Page1[0].c1_09[0]" => @general.spouse_birthday < "1956-01-02",
-          "topmostSubform[0].Page1[0].c1_10[0]" => @general.spouse_blind,
-          # "topmostSubform[0].Page1[0].c1_11[0]" => @general.dependents.size > 4,
+          # "topmostSubform[0].Page1[0].c1_07[0]" => @general.spouse_itemizes,
+          "topmostSubform[0].Page1[0].c1_08[0]" => @general.birthday < "1956-01-02",
+          "topmostSubform[0].Page1[0].c1_09[0]" => @general.blind,
+          "topmostSubform[0].Page1[0].c1_10[0]" => @general.spouse_birthday < "1956-01-02",
+          "topmostSubform[0].Page1[0].c1_11[0]" => @general.spouse_blind,
+          "topmostSubform[0].Page1[0].Dependents_ReadOrder[0].c1_12[0]" => @general.dependents.size > 4,
         }
       end
 
       def update_fields
         canvas = @doc.pages[0].canvas(type: :overlay)
-        canvas.font "Courier", variant: :bold, size: 12
+        canvas.font "Courier", size: 12, variant: :bold
 
         form = @doc.acro_form
         form_fields = []
@@ -84,7 +117,22 @@ module PiggyBank
           canvas.text value, at: [x + 6, y + 4]
         end
 
+        dependent_fields[:text].each do |name, value|
+          field = form_fields.find { |f| f.full_field_name == name }
+          raise "Cannot find PDF field #{name}" if field.nil?
+          x, y = field[:Rect]
+          canvas.text value, at: [x + 1, y + 2]
+        end
+
         button_fields.each do |name, value|
+          if value
+            field = form_fields.find { |f| f.full_field_name == name }
+            x, y = field[:Rect]
+            canvas.text "X", at: [x + 0.5, y + 0.5]
+          end
+        end
+
+        dependent_fields[:button].each do |name, value|
           if value
             field = form_fields.find { |f| f.full_field_name == name }
             x, y = field[:Rect]
