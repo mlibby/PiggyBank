6a01b1e142017680327c3acd557c7c58828e4a64
Michael C. Libby <m@mlibby.com>
2021-03-14 12:24:13 -0500

    fill out form 8889 from HSA data

--- src/lib/tax/form/adapter/us/form_8889.rb
+++ src/lib/tax/form/adapter/us/form_8889.rb
@@ -1,5 +1,82 @@
 require_relative "../base"
 
 class PiggyBank::Tax::Form::Adapter::US::Form8889 < PiggyBank::Tax::Form::Adapter::Base
+  def initialize
+    super
+    @hsa = PiggyBank::Tax::Data::HSA.instance
+  end
 
+  def covered_by_hdhp
+    _d @hsa.covered_by_hdhp
+  end
+
+  def line_2
+    _d("0")
+  end
+
+  def line_3
+    #FUTURE: not going to bother with the actual logic for this right now
+    _d("7100")
+  end
+
+  def line_4
+    _d("0")
+  end
+
+  def line_5
+    line_3 - line_4
+  end
+
+  def line_6
+    line_5
+  end
+
+  def line_7
+    _d("0")
+  end
+
+  def line_8
+    line_6 + line_7
+  end
+
+  def line_9
+    _d @hsa.f5968sa_contributions
+  end
+
+  def line_10
+    _d "0" 
+  end
+
+  def line_11
+    line_9 + line_10
+  end
+
+  def line_12
+    amount = line_8 - line_11
+    return amount < 0 ? _d("0.0") : amount
+  end
+
+  def line_13
+    line_2 < line_12 ? line_2 : line_12
+  end
+
+  def line_14a
+    _d @hsa.f1099sa_distributions
+  end
+
+  def line_14b
+    _d("0")
+  end
+
+  def line_14c
+    line_14a - line_14b
+  end
+
+  def line_15
+    _d @hsa.expenses_paid_by_hsa
+  end
+
+  def line_16 
+    line_14c - line_15
+  end
 end
--- src/lib/tax/form/writer/us/form_8889.rb
+++ src/lib/tax/form/writer/us/form_8889.rb
@@ -5,7 +5,7 @@ module PiggyBank::Tax::Form::Writer::US
     def initialize
       @template = "src/lib/tax/form/pdf/2020/us/f8889.pdf"
       super
-      @adapter = PiggyBank::Tax::Form::Adapter::US::ScheduleE.new
+      @adapter = PiggyBank::Tax::Form::Adapter::US::Form8889.new
     end
 
     private
@@ -19,15 +19,31 @@ module PiggyBank::Tax::Form::Writer::US
 
     def money_fields
       {
- #  "form1[0].Page1[0].f1_03[0]" => @format.as_currency(@adapter.line_1),
-               # "form1[0].Page1[0].f1_14[0]" => @format.as_currency(@adapter.line_9),
-        }
+        "topmostSubform[0].Page1[0].f1_03[0]" => @format.as_currency(@adapter.line_2, true),
+        "topmostSubform[0].Page1[0].f1_04[0]" => @format.as_currency(@adapter.line_3, true),
+        "topmostSubform[0].Page1[0].f1_05[0]" => @format.as_currency(@adapter.line_4, true),
+        "topmostSubform[0].Page1[0].f1_06[0]" => @format.as_currency(@adapter.line_5, true),
+        "topmostSubform[0].Page1[0].f1_07[0]" => @format.as_currency(@adapter.line_6, true),
+        "topmostSubform[0].Page1[0].f1_08[0]" => @format.as_currency(@adapter.line_7, true),
+        "topmostSubform[0].Page1[0].f1_09[0]" => @format.as_currency(@adapter.line_8, true),
+        "topmostSubform[0].Page1[0].f1_10[0]" => @format.as_currency(@adapter.line_9, true),
+        "topmostSubform[0].Page1[0].f1_11[0]" => @format.as_currency(@adapter.line_10, true),
+        "topmostSubform[0].Page1[0].f1_12[0]" => @format.as_currency(@adapter.line_11, true),
+        "topmostSubform[0].Page1[0].f1_13[0]" => @format.as_currency(@adapter.line_12, true),
+        "topmostSubform[0].Page1[0].f1_14[0]" => @format.as_currency(@adapter.line_13, true),
+        "topmostSubform[0].Page1[0].f1_15[0]" => @format.as_currency(@adapter.line_14a, true),
+        "topmostSubform[0].Page1[0].f1_16[0]" => @format.as_currency(@adapter.line_14b, true),
+        "topmostSubform[0].Page1[0].f1_17[0]" => @format.as_currency(@adapter.line_14c, true),
+        "topmostSubform[0].Page1[0].f1_18[0]" => @format.as_currency(@adapter.line_15, true),
+        "topmostSubform[0].Page1[0].f1_19[0]" => @format.as_currency(@adapter.line_16, true),
+      }
     end
 
     def button_fields
       {
- #"topmostSubform[0].Page1[0].FilingStatus[0].c1_01[0]" => @general.filing_status == "single",
-        }
+        "topmostSubform[0].Page1[0].c1_1[0]" => @adapter.covered_by_hdhp == "self",
+        "topmostSubform[0].Page1[0].c1_1[1]" => @adapter.covered_by_hdhp == "family",
+      }
     end
 
     def draw_fields
