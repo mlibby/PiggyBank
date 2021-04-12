93bd4a0ecf04ecb152b07f7bc76bb74ca4be6de6
Michael C. Libby <m@mlibby.com>
2021-04-12 17:31:33 -0500

    quick jade proof of concept

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
   
 app = Flask(__name__)
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
   
 @app.route("/")
 def home_view():
-  return "temporarily unavailable, back soon!"
+  return render_template('home/index.jade')
