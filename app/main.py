bf47ae52ebab60a1343260d66b4675708976eaef
Michael C. Libby <m@mlibby.com>
2021-04-12 20:57:02 -0500

    revive haml, semi-convert to pug, use pypugjs instead of pyjade

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
   
 app = Flask(__name__)
-app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
   
 @app.route("/")
 def home_view():
-  return render_template('home/index.jade')
+  return render_template('home/index.pug')
