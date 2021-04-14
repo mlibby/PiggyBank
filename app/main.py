c1e876312267da60fcc843cf230fa2447be5b565
Michael C. Libby <m@mlibby.com>
2021-04-14 16:38:16 -0500

    add emacs files to gitignore

--- app/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-  
-app = Flask(__name__)
-app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
-  
-@app.route("/")
-def home_view():
-  return render_template('home/index.pug')
