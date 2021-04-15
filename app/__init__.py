1e13ab52cc5f9c9b79101d58b34d0877f57f8932
Michael C. Libby <m@mlibby.com>
2021-04-15 15:33:03 -0500

    autopep8 experiment

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
-  
+
 app = Flask(__name__)
 app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
-  
+
+
 @app.route("/")
 def home_view():
-  return render_template('home/index.pug')
+    return render_template('home/index.pug')
+
 
 @app.route("/foo")
 def foo_view():
-  return "foo bar"
+    return "foo bar"
