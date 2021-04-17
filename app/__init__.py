42a54c76e43c419bfe487714c603b2a838882692
Michael C. Libby <m@mlibby.com>
2021-04-17 13:06:03 -0500

    move db update to main script

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,30 @@
 from flask import Flask, render_template
+# from ..repo import Repo
 from .routes import (
     home,
-#    accounts,
-#    budget,
-#    commodities,
-#    dashboard,
-#    ledger,
-#    ofx,
-#    prices,
-#    reports
+    #    accounts,
+    #    budget,
+    #    commodities,
+    #    dashboard,
+    #    ledger,
+    #    ofx,
+    #    prices,
+    #    reports
 )
 
 app = Flask(__name__,
-            static_url_path = '',
-            static_folder = 'static',
-            template_folder = 'templates')
+            static_url_path='',
+            static_folder='static',
+            template_folder='templates')
 
 app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
 app.url_map.strict_slashes = False
+
+# @home.before_app_first_request
+# def update_db():
+#     repo = Repo()
+#     repo.update_db()
+
 
 app.register_blueprint(home)
 # app.register_blueprint(accounts, url_prefix="/accounts")
@@ -28,6 +35,3 @@ app.register_blueprint(home)
 # app.register_blueprint(ofx, url_prefix="/ofx")
 # app.register_blueprint(prices, url_prefix="/prices")
 # app.register_blueprint(reports, url_prefix="/reports")
-
-
-
--- app/routes/home.py
+++ app/routes/home.py
@@ -1,12 +1,7 @@
 from flask import Blueprint, render_template
-# from ..repo import Repo        
 
 home = Blueprint("", __name__, template_folder="templates")
 
-# @home.before_app_first_request
-# def update_db():
-#     repo = Repo()
-#     repo.update_db()
 
 @home.route("/")
 def index():
