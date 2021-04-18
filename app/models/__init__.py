7a1b7d5215f4cd487a11f2bb7ac87f7623b782ce
Michael C. Libby <m@mlibby.com>
2021-04-17 20:15:02 -0500

    missing file

---
+++ app/models/__init__.py
@@ -0,0 +1,10 @@
+from .amortization import Amortization
+
+# class Amortization:
+#     def __init__(self, principal, rate, number):
+#         self.principal = principal
+#         self.rate = rate
+#         self.number = number
+
+#         self.payments = []
+#         self.payment_amount = 0
