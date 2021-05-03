c35842f2d123c293de98c57d1b07ffaa0c728ae4
Michael C. Libby <m@mlibby.com>
2021-05-03 15:14:54 -0500

    NavBar as Vue component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<div id="app">
+  <NavBar>
+  </NavBar>
+  <router-view/>
+</div>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  },
+};
+
+</script>
 
 <style>
 #app {
---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav role='navigation'>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
+
+<style>
+  nav { padding: 1px; }
+</style>
