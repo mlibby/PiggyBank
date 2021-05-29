6aaaee01876a39c6458d9326a909f02081d8646f
Michael C. Libby <m@mlibby.com>
2021-05-28 20:10:45 -0500

    fix demo db url

--- server/config.py
+++ server/config.py
@@ -4,6 +4,10 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, ".env"))
 
+db_url = os.environ.get("DATABASE_URL")
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+    os.environ["DATABASE_URL"] = db_url
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = (
