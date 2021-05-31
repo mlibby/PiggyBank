e3109d4dccbbc6a410635b00e8ca62855f376391
Michael C. Libby <m@mlibby.com>
2021-05-30 19:55:39 -0500

    trying to move account API calls to outer account route

--- client/src/views/account.vue
+++ client/src/views/account.vue
@@ -1,9 +1,51 @@
 <template>
-    <router-view></router-view>
+    <router-view
+        ref="accountView"
+        v-bind:account="account"
+        v-bind:accounts="accounts"
+        v-bind:account-types="accountTypes"
+    ></router-view>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     name: "Account",
+    data() {
+        return {
+            account: null,
+            accounts: [],
+            accountTypes: [],
+        };
+    },
+    mounted() {
+        this.$refs.accountView.load();
+    },
+    methods: {
+        async fetchAccount(next, error) {
+            const [accountsResponse, accountTypesResponse, accountReponse] =
+                await Promise.all([
+                    axios.get("/api/account/"),
+                    axios.get("/api/account/types"),
+                    axios.get("/api/account/" + this.$route.params.id),
+                ]).catch((e) => {
+                    error(e);
+                });
+
+            this.account = accountReponse.data;
+            this.accounts = accountsResponse.data;
+            this.accounts.sort((first, second) => {
+                return first.full_name.localeCompare(second.full_name);
+            });
+            const thisAccountIndex = this.accounts.findIndex((account) => {
+                account.id == this.account.id;
+            });
+            this.accounts.splice(thisAccountIndex, 1);
+            this.accountTypes = accountTypesResponse.data;
+
+            next();
+        },
+    },
 };
 </script>
--- client/src/views/account/edit.vue
+++ client/src/views/account/edit.vue
@@ -1,7 +1,7 @@
 <template>
     <main>
         <h1>Edit Account</h1>
-        <spinner-div ref="accountFormDiv" :onLoad="fetchAccount">
+        <spinner-div ref="accountFormDiv" :onLoad="loadAccount">
             <account-form
                 v-bind:account="account"
                 v-bind:accounts="accounts"
@@ -12,7 +12,6 @@
 </template>
 
 <script>
-import axios from "axios";
 import AccountForm from "@/components/account/form.vue";
 import SpinnerDiv from "../../components/SpinnerDiv.vue";
 
@@ -20,41 +19,10 @@ export default {
     name: "AccountEdit",
     title: "Edit Account",
     components: { AccountForm, SpinnerDiv },
-    data() {
-        return {
-            loading: true,
-            account: null,
-            accounts: [],
-            accountTypes: [],
-            error: null,
-        };
-    },
-    mounted() {
-        this.$refs.accountFormDiv.load();
-    },
+    props: ["account", "accounts", "accountTypes"],
     methods: {
-        async fetchAccount(next, error) {
-            const [accountsResponse, accountTypesResponse, accountReponse] =
-                await Promise.all([
-                    axios.get("/api/account/"),
-                    axios.get("/api/account/types"),
-                    axios.get("/api/account/" + this.$route.params.id),
-                ]).catch((e) => {
-                    error(e);
-                });
-
-            this.account = accountReponse.data;
-            this.accounts = accountsResponse.data;
-            this.accounts.sort((first, second) => {
-                return first.full_name.localeCompare(second.full_name);
-            });
-            const thisAccountIndex = this.accounts.findIndex((account) => {
-                account.id == this.account.id;
-            });
-            this.accounts.splice(thisAccountIndex, 1);
-            this.accountTypes = accountTypesResponse.data;
-
-            next();
+        loadAccount(next, error) {
+            this.$refs.accountFormDiv.load(next, error);
         },
     },
 };
