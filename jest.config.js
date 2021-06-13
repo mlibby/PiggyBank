8f56da761500e0648bc79e3b99953adfefe31f2e
Michael C. Libby <m@mlibby.com>
2021-06-12 21:58:07 -0500

    configure coverage for both jest and pytest

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,25 @@
 module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "client/src/**/*.{js,vue}",
+        "!client/src/main.js"
+    ],
+    coverageDirectory: "coverage-vue",
+    coverageReporters: ["html", "lcov"],
+
     // specifies aliases for imports
     moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/client/src/$1',
-        '^~/(.*)$': '<rootDir>/client/src/$1',
-        '^vue$': 'vue/dist/vue.common.js'
+        "^@/(.*)$": "<rootDir>/client/src/$1",
+        "^~/(.*)$": "<rootDir>/client/src/$1",
+        "^vue$": "vue/dist/vue.common.js",
     },
     // specifies extensions of files that will be tested
-    moduleFileExtensions: ['js', 'vue', 'json'],
+    moduleFileExtensions: ["js", "vue", "json"],
     // specifies a regular extension that will be used to find test files in your project
-    testRegex: '(/__tests__/.*|\\.(test|spec))\\.(js)$',
+    testRegex: "(/__tests__/.*|\\.(test|spec))\\.(js)$",
     // specifies that .js files must be transformed using babel-jest, and vue files must be transformed using vue-jest
     transform: {
-        '^.+\\.js$': 'babel-jest',
-        '.*\\.(vue)$': 'vue-jest'
-    }
-}
+        "^.+\\.js$": "babel-jest",
+        ".*\\.(vue)$": "vue-jest",
+    },
+};
