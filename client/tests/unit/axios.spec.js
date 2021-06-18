c0f0858a6ee1c41c92ded30363735eb5421bec6d
Michael C. Libby <m@mlibby.com>
2021-06-18 07:48:58 -0500

    remove useless axios files

--- client/tests/unit/axios.spec.js
+++
@@ -1,8 +0,0 @@
-import { shallowMount } from "@vue/test-utils";
-import _axios from "@/axios.js";
-
-describe("_axios.js", () => {
-    it("does this", () => {
-        expect(true).toBe(true);
-    });
-});
