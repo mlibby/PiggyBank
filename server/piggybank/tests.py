cbe7f5023ae43230776332e71667c006762d5db1
Michael C. Libby <m@mlibby.com>
2021-07-21 22:27:50 -0500

    serve index.html for /

--- server/piggybank/tests.py
+++ server/piggybank/tests.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 from django.urls import resolve
 from piggybank.views import home_page
 
+
 class HomePageTest(TestCase):
     def test_root_url_shows_spa_load_page(self):
-        found = resolve("/")
-        self.assertEqual(found.func, home_page)
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "index.html")
--- server/piggybank/views.py
+++ server/piggybank/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def home_page():
-    pass
+def home_page(request):
+    return render(request, "index.html")
