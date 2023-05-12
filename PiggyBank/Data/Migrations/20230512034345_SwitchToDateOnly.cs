6ee78d1e4c12b36a989b91d46319084b0220998b
Michael Libby <m@mlibby.com>
2023-05-12 17:13:16 -0500

    add migration files

---
+++ PiggyBank/Data/Migrations/20230512034345_SwitchToDateOnly.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PiggyBank.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SwitchToDateOnly : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnterDate",
+                table: "Transactions");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "PostDate",
+                table: "Transactions",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PostDate",
+                table: "Transactions",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EnterDate",
+                table: "Transactions",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
