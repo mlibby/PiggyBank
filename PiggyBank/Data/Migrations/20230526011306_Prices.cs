eb714288d11bd7df224c5a71da83e02f67491b81
Michael Libby <m@mlibby.com>
2023-05-27 08:07:19 -0500

    add enums to price migration

--- PiggyBank/Data/Migrations/20230526011306_Prices.cs
+++ PiggyBank/Data/Migrations/20230526011306_Prices.cs
@@ -11,6 +11,60 @@ public partial class Prices : Migration
     protected override void Up(MigrationBuilder migrationBuilder)
     {
         migrationBuilder.CreateTable(
+            name: "DataSource",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "INTEGER", nullable: false),
+                Text = table.Column<string>(type: "TEXT", maxLength: 2048, nullable: false)
+            },
+            constraints: table => table.PrimaryKey("PK_DataSource", x => x.Id));
+
+        foreach (var e in Enum.GetNames(typeof(DataSource.DataSourceType)))
+        {
+            migrationBuilder.InsertData(
+                table: "DataSource",
+                columns: new[] { "Id", "Text" },
+                columnTypes: new[] { "INTEGER", "TEXT" },
+                values: new object[] { (int)Enum.Parse(typeof(DataSource.DataSourceType), e), e });
+        }
+
+        migrationBuilder.CreateTable(
+            name: "Price_PriceSource",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "INTEGER", nullable: false),
+                Text = table.Column<string>(type: "TEXT", maxLength: 2048, nullable: false)
+            },
+            constraints: table => table.PrimaryKey("PK_Price_PriceSource", x => x.Id));
+
+        foreach (var e in Enum.GetNames(typeof(Price.PriceSourceType)))
+        {
+            migrationBuilder.InsertData(
+                table: "Price_PriceSource",
+                columns: new[] { "Id", "Text" },
+                columnTypes: new[] { "INTEGER", "TEXT" },
+                values: new object[] { (int)Enum.Parse(typeof(Price.PriceSourceType), e), e });
+        }
+
+        migrationBuilder.CreateTable(
+            name: "Price_PriceType",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "INTEGER", nullable: false),
+                Text = table.Column<string>(type: "TEXT", maxLength: 2048, nullable: false)
+            },
+            constraints: table => table.PrimaryKey("PK_Price_PriceType", x => x.Id));
+
+        foreach (var e in Enum.GetNames(typeof(Price.PriceType)))
+        {
+            migrationBuilder.InsertData(
+                table: "Price_PriceType",
+                columns: new[] { "Id", "Text" },
+                columnTypes: new[] { "INTEGER", "TEXT" },
+                values: new object[] { (int)Enum.Parse(typeof(Price.PriceType), e), e });
+        }
+
+        migrationBuilder.CreateTable(
             name: "Prices",
             columns: table => new
             {
@@ -31,7 +85,35 @@ public partial class Prices : Migration
                     principalTable: "Commodities",
                     principalColumn: "Id",
                     onDelete: ReferentialAction.Cascade);
+                table.ForeignKey(
+                    name: "FK_Prices_DataSource",
+                    column: x => x.Source,
+                    principalTable: "DataSource",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+                table.ForeignKey(
+                    name: "FK_Prices_PriceSource",
+                    column: x => x.PriceSource,
+                    principalTable: "Price_PriceSource",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+                table.ForeignKey(
+                    name: "FK_Prices_PriceType",
+                    column: x => x.Type,
+                    principalTable: "Price_PriceType",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
             });
+
+        migrationBuilder.CreateIndex(
+            name: "IX_Prices_PriceSource",
+            table: "Prices",
+            column: "PriceSource");
+
+        migrationBuilder.CreateIndex(
+            name: "IX_Prices_PriceType",
+            table: "Prices",
+            column: "PriceType");
 
         migrationBuilder.CreateIndex(
             name: "IX_Prices_CommodityId",
@@ -42,7 +124,8 @@ public partial class Prices : Migration
     /// <inheritdoc />
     protected override void Down(MigrationBuilder migrationBuilder)
     {
-        migrationBuilder.DropTable(
-            name: "Prices");
+        migrationBuilder.DropTable(name: "Prices");
+        migrationBuilder.DropTable(name: "Price_PriceSource");
+        migrationBuilder.DropTable(name: "Price_PriceType");
     }
 }
